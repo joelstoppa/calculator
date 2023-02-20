321de3ccbb8a506c4e2594ca22e4d8eb59e4f925
joelstoppa <joel.stoppa@gmail.com>
2023-02-20 13:22:59 +0100

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,25 @@
+const num1 = 5
+const num2 = 10
+
+function add(num1, num2) {
+    return (num1 + num2)
+}
+
+console.log(add(num1, num2))
+
+function subtract(num1, num2) {
+    return (num1 - num2)
+}
+console.log(subtract(num1, num2))
+
+function multiply (num1, num2) {
+    return (num1 * num2)
+}
+
+console.log(multiply(num1, num2))
+
+function divide(num1, num2) {
+    return (num1 / num2)
+}
+
+console.log(divide(num2,num1))
