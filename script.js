8cb6613dd3be348b8e92e39cab8d297610f87335
joelstoppa <joel.stoppa@gmail.com>
2023-02-20 13:47:41 +0100

    Add buttons with querySelector

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-const num1 = 5
-const num2 = 10
+const numberOneButton = document.querySelector('#numberOneButton');
+const numberTwoButton = document.querySelector('#numberTwoButton');
+const numberThreeButton = document.querySelector('#numberThreeButton');
+const numberFourButton = document.querySelector('#numberFourButton');
+const numberFiveButton = document.querySelector('#numberFiveButton');
+const numberSixButton = document.querySelector('#numberSixButton');
+const numberSevenButton = document.querySelector('#numberSevenButton');
+const numberEightThreeButton = document.querySelector('#numberEightButton');
+const numberNineButton = document.querySelector('#numberNineButton');
+const numberZeroButton = document.querySelector('#numberZeroButton');
+const resultButton = document.querySelector('#resultButton');
+const plusButton = document.querySelector('#plusButton');
+const minusButton = document.querySelector('#minusButton');
+const multiplyButton = document.querySelector('#mulitplyButton');
+const divideButton = document.querySelector('#divideButton');
+const percentButton = document.querySelector('#percentButton');
+const plusMinusButton = document.querySelector('#plusMinusButton');
+const clearButton = document.querySelector('#clearButton');
+
 
 function add(num1, num2) {
     return (num1 + num2)
 }
 
-console.log(add(num1, num2))
-
 function subtract(num1, num2) {
     return (num1 - num2)
 }
-console.log(subtract(num1, num2))
 
 function multiply (num1, num2) {
     return (num1 * num2)
 }
 
-console.log(multiply(num1, num2))
-
 function divide(num1, num2) {
     return (num1 / num2)
 }
-
-console.log(divide(num2,num1))
